1899ee3d996578682fd778d64fc89635c235550d
Alexander Whitaker <whitaker.alexander@gmail.com>
2016-08-05 11:42:09 -0500

    placed promptResponse inside letsAGo

--- madlib.js
+++ madlib.js
@@ -1,6 +1,5 @@
-var promptResponse = prompt("Are you ready for this exciting MadLib? Yes or No", "Yes");
-
-var letsAGo = function(promptResponse) {
+var letsAGo = function() {
+    var promptResponse = prompt("Are you ready for this exciting MadLib? Yes or No", "Yes");
     if (promptResponse === "Yes") {
       alert("Yay! Let's a go!");
       madLib();
@@ -8,8 +7,6 @@ var letsAGo = function(promptResponse) {
         alert("Boo!");
     }
 };
-
-letsAGo(promptResponse);
 
 function madLib() {
     var madLibObj = {
@@ -38,6 +35,4 @@ function madLib() {
     console.log("One of the " +  madLibObj.adj1 + " things about summer is that my " + madLibObj.pluralNoun + " are " + madLibObj.ingVerb + " a huge party! I decided to throw a back yard " + madLibObj.noun1 + " for all of my family and " + madLibObj.pluralNoun2 + ". I have invited my best friend " + madLibObj.celebrityFemale + ", " + madLibObj.maleInRoom + ", and of course my neighbor, Ms. " + madLibObj.sillyWord + ". My dad is going to " + madLibObj.sillyVerb + " hamburgers and " + madLibObj.typeOfFoodPlural + " on his shiny new " + noun2 + ". He always thinks his " + madLibObj.typeOfFood + " tastes really " + madLibObj.adj2 + ", but I think they taste like " + madLibObj.adj3 + " " + madLibObj.typeOfShoes + ". My mom is going to make her famous " + madLibObj.somethingAlive + " salad, which is my favorite " + madLibObj.noun3 + " ever! My mom said, after we finish " + madLibObj.ingVerb2 + ", we can go swimming in our new " + madLibObj.adj4 + " " + madLibObj.sillyNoun + "!");
 };
 
-
-
-
+letsAGo();
